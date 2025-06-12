a7b10dcc439053b9d02a1d64d9fb7f94345c5fa9
Daniil Pokrovsky <daniil.pokrovsky@gmail.com>
2025-06-12 12:30:52 +0700

    Support paths start with tilda

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,5 +1,6 @@
 // Used for parsing a transform module.
 import 'ts-node/register/transpile-only'
+import os from 'os'
 import path from 'path'
 
 import { Command } from 'commander'
@@ -15,98 +16,103 @@ import { Codemod, CodemodContext, TransformOptions } from './types'
 const program = new Command()
 
 interface CodemodCliOptions extends TransformOptions {
-    write: boolean
-    format: boolean
-    transform: string
+  write: boolean
+  format: boolean
+  transform: string
 }
 
 const PROJECT_ROOT = path.resolve(__dirname, '../../../')
 
 program
-    // TODO: make it `true` by default after switching to fast bulk format.
-    .option('-f, --format [format]', 'Format Typescript source files with ESLint', false)
-    .option('-w, --write [write]', 'Persist codemod changes to the filesystem', false)
-    .option('-t, --transform <transform>', 'Absolute or relative to project root path to a transform module')
-    .argument('<fileGlob>', 'Absolute or relative to project root file glob to change files based on')
-    .allowUnknownOption(true)
-    .enablePositionalOptions(true)
-    .addHelpText(
-        'after',
-        dedent`
+  // TODO: make it `true` by default after switching to fast bulk format.
+  .option('-f, --format [format]', 'Format Typescript source files with ESLint', false)
+  .option('-w, --write [write]', 'Persist codemod changes to the filesystem', false)
+  .option('-t, --transform <transform>', 'Absolute or relative to project root path to a transform module')
+  .argument('<fileGlob>', 'Absolute or relative to project root file glob to change files based on')
+  .allowUnknownOption(true)
+  .enablePositionalOptions(true)
+  .addHelpText(
+    'after',
+    dedent`
 
         Transform-specific options can be passed via using '--option=value' syntax:
         yarn transform --write --tagToConvert=Link -t ./transformPath.ts 'globPath/**/*.{ts,tsx}'
     `
-    )
-    .action(async (commandArgument: string, options: CodemodCliOptions) => {
-        const { fileGlob, transformOptions } = parseOptions(commandArgument)
-        const { write: shouldWriteFiles, format: shouldFormat, transform } = options
+  )
+  .action(async (commandArgument: string, options: CodemodCliOptions) => {
+    const { fileGlob, transformOptions } = parseOptions(commandArgument)
+    const { write: shouldWriteFiles, format: shouldFormat, transform } = options
 
-        const projectGlob = path.isAbsolute(fileGlob) ? fileGlob : path.join(PROJECT_ROOT, fileGlob)
-        const transformPath = path.isAbsolute(transform) ? transform : path.join(PROJECT_ROOT, transform)
+    // Handle tilde expansion
+    const expandedFileGlob = fileGlob.startsWith('~')
+      ? path.join(os.homedir(), fileGlob.slice(1))
+      : fileGlob
 
-        signale.start(`Starting codemod "${transformPath}" with the project glob "${projectGlob}".`)
+    const projectGlob = path.isAbsolute(expandedFileGlob) ? expandedFileGlob : path.join(PROJECT_ROOT, expandedFileGlob)
+    const transformPath = path.isAbsolute(transform) ? transform : path.join(PROJECT_ROOT, transform)
 
-        const project = new Project()
-        project.addSourceFilesAtPaths(projectGlob)
+    signale.start(`Starting codemod "${transformPath}" with the project glob "${projectGlob}".`)
 
-        const transformExports = Object.values(await import(transformPath))
-        if (transformExports.length !== 1) {
-            throw new Error('Transform file should have one named export with the transform function.')
-        }
+    const project = new Project()
+    project.addSourceFilesAtPaths(projectGlob)
 
-        const [codemod] = transformExports
-        const codemodContext: CodemodContext = {
-            project,
-            shouldWriteFiles,
-            shouldFormat,
-            transformOptions,
-        }
+    const transformExports = Object.values(await import(transformPath))
+    if (transformExports.length !== 1) {
+      throw new Error('Transform file should have one named export with the transform function.')
+    }
 
-        const results = await (codemod as Codemod)(codemodContext)
+    const [codemod] = transformExports
+    const codemodContext: CodemodContext = {
+      project,
+      shouldWriteFiles,
+      shouldFormat,
+      transformOptions,
+    }
 
-        results.map(result => {
-            logRequiredManualChanges(result.manualChangesReported)
-        })
+    const results = await (codemod as Codemod)(codemodContext)
 
-        if (shouldWriteFiles) {
-            signale.await('Persisting codemod changes to the filesystem...')
-            await Promise.all(
-                results.map(result => {
-                    return result.fsWritePromise
-                })
-            )
-            signale.complete('Persisting codemod changes completed.')
-        } else {
-            for (const file of results.flatMap(result => {
-                return result.files
-            })) {
-                signale.log(file?.source)
-            }
-        }
-
-        signale.success('Codemod is applied!')
+    results.map(result => {
+      logRequiredManualChanges(result.manualChangesReported)
     })
+
+    if (shouldWriteFiles) {
+      signale.await('Persisting codemod changes to the filesystem...')
+      await Promise.all(
+        results.map(result => {
+          return result.fsWritePromise
+        })
+      )
+      signale.complete('Persisting codemod changes completed.')
+    } else {
+      for (const file of results.flatMap(result => {
+        return result.files
+      })) {
+        signale.log(file?.source)
+      }
+    }
+
+    signale.success('Codemod is applied!')
+  })
 
 program.parse(process.argv)
 
 interface ParseOptionsResult {
-    fileGlob: string
-    transformOptions: TransformOptions
+  fileGlob: string
+  transformOptions: TransformOptions
 }
 
 function parseOptions(commandArgument: string): ParseOptionsResult {
-    const { unknown } = program.parseOptions(process.argv)
+  const { unknown } = program.parseOptions(process.argv)
 
-    // TODO: find a better way to process and validate `transformOptions`.
-    const fileGlob = unknown.pop() || commandArgument
+  // TODO: find a better way to process and validate `transformOptions`.
+  const fileGlob = unknown.pop() || commandArgument
 
-    const transformOptions = unknown.reduce<Record<string, unknown>>((result, key) => {
-        const [name, value = true] = key.split('=')
-        result[name.replace('--', '')] = value
+  const transformOptions = unknown.reduce<Record<string, unknown>>((result, key) => {
+    const [name, value = true] = key.split('=')
+    result[name.replace('--', '')] = value
 
-        return result
-    }, {})
+    return result
+  }, {})
 
-    return { fileGlob, transformOptions }
+  return { fileGlob, transformOptions }
 }
